18f7cbf4aed56617af62993b629d3600a2349ece
Océane Devautour <oceanedevautour@mbp-de-oceane.home>
2024-10-22 20:09:12 +0200

    correction nuancier et optimisation des variable coulleur

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import "./Contact.scss";
 
+
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: '',
